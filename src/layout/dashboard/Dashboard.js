d0673cae21e8ed2e922360e0aa7f80f29e51b413
fsoeltoni <fsoeltoni@gmail.com>
2020-02-02 18:12:39 +0700

    Modify dashboard

--- src/layout/dashboard/Dashboard.js
+++ src/layout/dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { useDataProvider } from "react-admin";
+import moment from "moment";
 
 const Dashboard = () => {
   const [state, setState] = useState({});
@@ -11,9 +12,30 @@ const Dashboard = () => {
       pagination: { page: 1, perPage: 50000000 }
     });
 
+    const aggregations = allSim
+      .filter(
+        sim => moment(sim.created).format("YYYY") === moment().format("YYYY")
+      )
+      .reduce(
+        (stats, sim) => {
+          stats.simThnIni.push(sim);
+
+          if (moment(sim.created).format("M") === moment().format("M")) {
+            stats.simBlnIni.push(sim);
+          }
+
+          return stats;
+        },
+        {
+          simThnIni: [],
+          simBlnIni: []
+        }
+      );
+
     setState(state => ({
       ...state,
-      allSim
+      simThnIni: aggregations.simThnIni,
+      simBlnIni: aggregations.simBlnIni
     }));
   }, [dataProvider]);
 
